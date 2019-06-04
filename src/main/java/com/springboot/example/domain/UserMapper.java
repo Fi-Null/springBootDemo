419b8af70f9d7445c1ab304a039391c455f6c8a8
向科 <xiangke6@jd.com>
2019-06-04 11:26:09 +0800

    demo

---
+++ src/main/java/com/springboot/example/domain/UserMapper.java
@@ -0,0 +1,45 @@
+package com.springboot.example.domain;
+
+import org.apache.ibatis.annotations.*;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author null
+ * @version 1.0
+ * @title
+ * @description
+ * @createDate 6/4/19 10:22 AM
+ */
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM user WHERE name = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Results({
+            @Result(property = "name", column = "name"),
+            @Result(property = "age", column = "age")
+    })
+    @Select("SELECT name, age FROM user")
+    List<User> findAll();
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+
+    @Update("UPDATE user SET age=#{age} WHERE name=#{name}")
+    void update(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name}, #{age})")
+    int insertByUser(User user);
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name,jdbcType=VARCHAR}, #{age,jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+
+}
--- src/test/java/com/springboot/example/ConfigApplicationTests.java
+++ src/test/java/com/springboot/example/ConfigApplicationTests.java
@@ -4,6 +4,8 @@ import com.springboot.example.service.BlogProperties;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,10 +20,18 @@ public class ConfigApplicationTests {
 	@Autowired
 	private BlogProperties blogProperties;
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Test
 	public void getHello() throws Exception {
 		Assert.assertEquals(blogProperties.getName(), "Programmer AA");
 		Assert.assertEquals(blogProperties.getTitle(), "Spring Boot Demo");
+	}
+
+	@Test
+	public void test() throws Exception {
+		logger.info("输出info");
+		logger.debug("输出debug");
+		logger.error("输出error");
 	}
 }
